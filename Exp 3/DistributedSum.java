32ac4edc805ec15437163b376af446e7e79153c1
SairajHake1 <shubhambangal03@gmail.com>
2025-05-07 00:23:53 +0530

    Update DistributedSum.java

--- Exp 3/DistributedSum.java
+++ Exp 3/DistributedSum.java
@@ -1,31 +1,32 @@
 import mpi.*;
 
-public class DistributedSum {
+public class ScatterGather {
     public static void main(String[] args) throws MPIException {
         MPI.Init(args);
 
-        int rank = MPI.COMM_WORLD.Rank(); // Rank of current process
-        int size = MPI.COMM_WORLD.Size(); // Total number of processes
+        int rank = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
 
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Sample array
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Can be any size
 
-        int localSum = 0; // Local sum for each process
-        int[] recvBuffer = new int[1]; // Buffer for receiving sum from other processes
+        int totalLength = array.length;
+        int chunkSize = totalLength / size;
+        int remainder = totalLength % size;
 
-        // Distribute elements to different processes
-        int startIndex = rank * (array.length / size);
-        int endIndex = (rank + 1) * (array.length / size);
+        // Compute start and end indices for each process
+        int startIndex = rank * chunkSize + Math.min(rank, remainder);
+        int endIndex = startIndex + chunkSize + (rank < remainder ? 1 : 0);
+
+        int localSum = 0;
         for (int i = startIndex; i < endIndex; i++) {
             localSum += array[i];
         }
 
-        // Print intermediate sums calculated by each process
         System.out.println("Process " + rank + " intermediate sum: " + localSum);
 
-        // Reduce local sums to find global sum
+        int[] recvBuffer = new int[1];
         MPI.COMM_WORLD.Reduce(new int[]{localSum}, 0, recvBuffer, 0, 1, MPI.INT, MPI.SUM, 0);
 
-        // Process 0 prints the final sum
         if (rank == 0) {
             System.out.println("Final sum: " + recvBuffer[0]);
         }
